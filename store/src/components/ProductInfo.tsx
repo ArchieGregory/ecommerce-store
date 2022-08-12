f01d03f7ed277d1be779836920c4c5ca5234736c
Archie <a.j.o.gregory@gmail.com>
2022-08-12 17:01:53 +0100

    Created Product Info Component

---
+++ store/src/components/ProductInfo.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+
+interface Product {
+    "id": string,
+    "item_name": string,
+    "item_unit_cost": number,
+    "item_unit_weight": string,
+    "type": string
+}
+
+
+interface ProductInfoProps {
+    product: Product,
+}
+
+
+function ProductInfo ({product}: ProductInfoProps) {
+
+
+    return (
+        <div className='card'>
+            <h1>
+                {product.item_name}
+            </h1>
+            <p>Weight: {product.item_unit_weight}</p>
+            <p>Cost: {product.item_unit_cost}</p>
+            <p>Type: {product.type}</p>
+            <button>Add to cart</button> 
+        </div>
+     
+
+    );
+}
+
+export default ProductInfo;
